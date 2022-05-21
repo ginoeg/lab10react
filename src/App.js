ffd04f8e05f183f47b79fe84187aae57dc932a1b
AlexanderF <alexander.flores.r@tecsup.edu.pe>
2022-05-21 00:45:53 -0500

    Lab10React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: [],
+      recuperado:true
+    }
+}
+
+componentWillMount() {
+  fetch('http://127.0.0.1:8000/productos/')
+    .then((response) => {
+      return response.json()
+    })
+    .then((prod) => {
+      this.setState({ productos: prod,recuperado:true})
+    })
+} 
+
+render() {
+    if (this.state.recuperado){
+      return this.mostrarTabla()
+    }else{
+      return (<div>recuperando datos...</div>)
+  }
+}
+
+mostrarTabla() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table border="1">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Descripción</th>
+          <th>Precio</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.productos.map(prod => {
+          return (
+            <tr key={prod.id}>
+              <td>{prod.id}</td>
+              <td>{prod.descripcion}</td>
+              <td>{prod.precio}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+      </table>
     </div>
   );
 }
-
+  borrar(ide) {
+    var temp = this.state.articulos.filter((el)=>el.id !== ide);
+    this.setState({
+      articulos: temp
+    })
+  }
+}
 export default App;
